271b8d593d8a2757236c6d86d1e2644160c12c7c
thobbiz <odelolatojumi@gmail.com>
2025-10-20 16:02:31 +0100

    feat: added mapDb

--- main.go
+++ main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	str := "string to analyze"
-	fmt.Println(wordCount(str))
+	router := gin.Default()
+	router.POST("/strings", createString)
+	router.Run(":7070")
+}
+
+var ObjectDB map[string]ObjectString
+
+type ObjectString struct {
+	Id         string     `json:"id"`
+	Value      string     `json:"string"`
+	Properties Properties `json:"properties"`
+	CreatedAt  string     `json:"created_at"`
+}
+
+type Properties struct {
+	StringLength     int            `json:"length"`
+	IsPalindrome     bool           `json:"is_palindrome"`
+	UniqueCharacater int            `json:"unique_characters"`
+	WordCount        int            `json:"word_count"`
+	Hash             string         `json:"sha256_hash"`
+	CharFreqMap      map[string]int `json:"character_frequency_map"`
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
 }
